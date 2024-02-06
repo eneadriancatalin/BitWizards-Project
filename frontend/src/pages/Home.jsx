b23c582536a5428c3b5a98be5459ea79a0a45046
Ene Adrian <71841822+eneadriancatalin@users.noreply.github.com>
2024-02-06 11:53:44 +0200

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -9,6 +9,69 @@ const Home = () => {
       <div className="space-y-10">
         {/* //! TITLUL PAGINII */}
         <Title titlu="Acasa" />
+        <div className="grid grid-cols-4 gap-x-16">
+          <Block
+            tag={
+              <div
+                className={
+                  "uppercase text-[#1D1D1B] font-bold text-sm px-3 py-1 max-w-max rounded-md tracking-normal bg-[#F4F5F7] ring-1 ring-[#DEDFDF]"
+                }
+              >
+                Numar de teste
+              </div>
+            }
+            numar={data.tests.filter((element) => element.user === nume).length}
+          />
+          <Block
+            tag={
+              <div
+                className={
+                  "uppercase text-[#1D1D1B] font-bold text-sm px-3 py-1 max-w-max rounded-md tracking-normal bg-[#9794F7]"
+                }
+              >
+                Teste in configurare
+              </div>
+            }
+            numar={
+              data.tests.filter(
+                (element) => element.user === nume && element.status === 0
+              ).length
+            }
+          />
+          <Block
+            tag={
+              <div
+                className={
+                  "uppercase text-[#1D1D1B] font-bold text-sm px-3 py-1 max-w-max rounded-md tracking-normal bg-[#A1E0DE]"
+                }
+              >
+                Teste active
+              </div>
+            }
+            numar={
+              data.tests.filter(
+                (element) => element.user === nume && element.status === 1
+              ).length
+            }
+          />
+          <Block
+            tag={
+              <div
+                className={
+                  "uppercase text-[#1D1D1B] font-bold text-sm px-3 py-1 max-w-max rounded-md tracking-normal bg-[#F7CE78]"
+                }
+              >
+                Teste terminate
+              </div>
+            }
+            numar={
+              data.tests.filter(
+                (element) => element.user === nume && element.status === -1
+              ).length
+            }
+          />
+        </div>
+        >>>>>>> Stashed changes
       </div>
     </div>
   );
